87020f5c47ebe054bcba3c602230be058a00d919
LouisCab <lo.cabirol@gmail.com>
2022-10-10 23:00:21 +0200

    feat(scrapper): dependency injection

--- src/scrap.module.ts
+++ src/scrap.module.ts
@@ -1,9 +1,73 @@
-import { Module } from '@nestjs/common';
+import { FactoryProvider, Module } from '@nestjs/common';
 import { GetCompanyInformationsController } from 'src/infrastructure/controller/get-company-informations.controller';
+import { GetCompanyInformationsService } from './application/services/get-company-informations.service';
+import { LinkedinPuppeteerInformationCrawler } from './infrastructure/crawler/linkedin.puppeteer.crawler';
+import { SocieteComPuppeteerInformationCrawler } from './infrastructure/crawler/societe-com.puppeteer.crawler';
+import { LinkedinPuppeteerInformationExtractor } from './infrastructure/extractor/linkedin.puppeteer.extractor';
+import { SocieteComPuppeteerInformationExtractor } from './infrastructure/extractor/societe-com.puppeteer.extractor';
+import { LinkedinInformationProvider } from './infrastructure/provider/linkedin.provider';
+import { SocieteComInformationProvider } from './infrastructure/provider/societe-com.provider';
+import { linkedinInformationReferential } from './infrastructure/referential/linkedin.referential';
+import { societeComInformationReferential } from './infrastructure/referential/societe-com.referential';
+
+const crawlers = [
+  {
+    provide: LinkedinPuppeteerInformationCrawler,
+    useClass: LinkedinPuppeteerInformationCrawler,
+  },
+  {
+    provide: SocieteComPuppeteerInformationCrawler,
+    useClass: SocieteComPuppeteerInformationCrawler,
+  },
+];
+
+const linkedinProvider = {
+  provide: LinkedinInformationProvider,
+  inject: [LinkedinPuppeteerInformationCrawler],
+  useFactory: (crawler: LinkedinPuppeteerInformationCrawler) => {
+    return new LinkedinInformationProvider(
+      new LinkedinPuppeteerInformationExtractor(
+        linkedinInformationReferential,
+        crawler,
+      ),
+      crawler,
+    );
+  },
+};
+
+const societeComProvider = {
+  provide: SocieteComInformationProvider,
+  inject: [SocieteComPuppeteerInformationCrawler],
+  useFactory: (crawler: SocieteComPuppeteerInformationCrawler) => {
+    return new SocieteComInformationProvider(
+      new SocieteComPuppeteerInformationExtractor(
+        societeComInformationReferential,
+        crawler,
+      ),
+      crawler,
+    );
+  },
+};
+
+const getCompanyInformationsService: FactoryProvider = {
+  provide: GetCompanyInformationsService,
+  inject: [LinkedinInformationProvider, SocieteComInformationProvider],
+  useFactory: (
+    linkedin: LinkedinInformationProvider,
+    societeCom: SocieteComInformationProvider,
+  ) => {
+    return new GetCompanyInformationsService([linkedin, societeCom]);
+  },
+};
 
 @Module({
   imports: [],
   controllers: [GetCompanyInformationsController],
-  providers: [],
+  providers: [
+    ...crawlers,
+    linkedinProvider,
+    societeComProvider,
+    getCompanyInformationsService,
+  ],
 })
 export class AppModule {}
